34169d925d93c84d241983a86ab78b5c095aeca8
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-22 16:46:07 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int toplamtek = 0;
+            int toplamcift = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if(i%2==1)
+                {
+                    toplamtek +=i;
+                    continue;
+                }
+                else
+                {
+                    toplamcift += i;
+
+                }
+                if(i== 500)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("tek toplam "+ toplamtek);
+            Console.WriteLine("çift toplam "+ toplamcift);
+        
+         
+        
+
+        }
+        
+      
+    }
+    
+}
